3a5c88094c30d22107750df35b7030c65513ad74
simon <simonaertsbecode@gmail.com>
2021-10-11 10:03:42 +0200

    bug resolved

--- client/src/components/forms/log/SignInForm.js
+++ client/src/components/forms/log/SignInForm.js
@@ -1,114 +1,112 @@
 import React, { useContext, useState } from 'react';
-import axios from 'axios'
-import { UserContext } from '../../../context/UserContext'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
-import e from 'express';
+// import axios from 'axios';
+import { UserContext } from '../../../context/UserContext';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const SignInForm = () => {
+   const [signInFormValues, setSignInFormValues] = useContext(UserContext);
 
-  const [signInFormValues, setSignInFormValues] = useContext(UserContext)
+   const [passwordError, setPasswordError] = useState(false);
 
-  const [passwordError, setPasswordError] = useState(false)
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      setSignInFormValues((prevState) => ({
+         ...prevState,
+         [name]: value,
+      }));
+   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target
-    setSignInFormValues(prevState => ({
-          ...prevState,
-          [name]: value
-      }))
-    }
-  
-  // const submitForm = () =>{
-  //   e.preventDefault()
-  //   if(signInFormValues.password !== signInFormValues.password2){
-  //     setPasswordError(true)
-  //   } else {
-  //     setPasswordError(false)
-  //     axios.post('/register', signInFormValues)
-  //       .then(res => {
-  //         console.log(res)
-  //       })
-  //       .catch(err =>{
-  //         console.log(err)
-  //       })
-  //   }
-  // }  
-  
+   //  const submitForm = () => {
+   //     e.preventDefault();
+   //     if (signInFormValues.password !== signInFormValues.password2) {
+   //        setPasswordError(true);
+   //     } else {
+   //        setPasswordError(false);
+   //        axios
+   //           .post('/register', signInFormValues)
+   //           .then((res) => {
+   //              console.log(res);
+   //           })
+   //           .catch((err) => {
+   //              console.log(err);
+   //           });
+   //     }
+   //  };
 
-  return (
-    <div className='sigIn-Form'>
-      <form>
-        <TextField 
-            className='field'
-            name='name'
-            value={signInFormValues.name}
-            label='Nom'
-            variant='outlined'
-            required
-            fullWidth
-            onChange={handleChange}
-          />
-          <strong></strong>
-          <TextField 
-            className='field'
-            name='firstName'
-            value={signInFormValues.firstName}
-            label='Prénom'
-            variant='outlined'
-            required
-            fullWidth
-            onChange={handleChange}
-          />
-          <strong></strong>
-          <TextField 
-            className='field'
-            name='email'
-            value={signInFormValues.email}
-            label='email'
-            variant='outlined'
-            required
-            fullWidth
-            onChange={handleChange}
-          />
-          <strong></strong>
-          <TextField 
-            className='field'
-            name='password'
-            value={signInFormValues.password}
-            label='Mot de passe'
-            variant='outlined'
-            type='password'
-            required
-            fullWidth
-            onChange={handleChange}
-          />
-          <strong></strong>
-          <TextField 
-            className='field'
-            value={signInFormValues.password2}
-            label='Mot de passe'
-            variant='outlined'
-            type='password'
-            required
-            fullWidth
-            onChange={handleChange}
-          />
-          <strong>{passwordError ? 'Vos deux mots de passe ne correspondent pas' : ''}</strong>
-          <Button 
-            className='btn-submit'
-            // onClick={submitForm}
-            type='submit'
-            color='rgba(0,61,217)'
-            variant='contained'
-            endIcon={<KeyboardArrowRightIcon />}
-          >
-            Enregistrez-vous
-          </Button>
-      </form>
-    </div>
-  );
+   return (
+      <div className='sigIn-Form'>
+         <form>
+            <TextField
+               className='field'
+               name='name'
+               value={signInFormValues.name}
+               label='Nom'
+               variant='outlined'
+               required
+               fullWidth
+               onChange={handleChange}
+            />
+            <strong></strong>
+            <TextField
+               className='field'
+               name='firstName'
+               value={signInFormValues.firstName}
+               label='Prénom'
+               variant='outlined'
+               required
+               fullWidth
+               onChange={handleChange}
+            />
+            <strong></strong>
+            <TextField
+               className='field'
+               name='email'
+               value={signInFormValues.email}
+               label='email'
+               variant='outlined'
+               required
+               fullWidth
+               onChange={handleChange}
+            />
+            <strong></strong>
+            <TextField
+               className='field'
+               name='password'
+               value={signInFormValues.password}
+               label='Mot de passe'
+               variant='outlined'
+               type='password'
+               required
+               fullWidth
+               onChange={handleChange}
+            />
+            <strong></strong>
+            <TextField
+               className='field'
+               value={signInFormValues.password2}
+               label='Mot de passe'
+               variant='outlined'
+               type='password'
+               required
+               fullWidth
+               onChange={handleChange}
+            />
+            <strong>{passwordError ? 'Vos deux mots de passe ne correspondent pas' : ''}</strong>
+            <Button
+               className='btn-submit'
+               // onClick={submitForm}
+               type='submit'
+               color='rgba(0,61,217)'
+               variant='contained'
+               endIcon={<KeyboardArrowRightIcon />}
+            >
+               Enregistrez-vous
+            </Button>
+         </form>
+      </div>
+   );
 };
 
 export default SignInForm;
--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,31 +1,28 @@
-import React, {createContext, useState} from 'react'
+import React, { createContext, useState } from 'react';
 
 export const UserContext = createContext();
 
-export const UserProvider = props => {
-  const [initialValues, setValue] = useState({
-    name: '',
-    firstName: '',
-    email: '',
-    age: '',
-    sex: '',
-    content: ''
-  })
+export const UserProvider = (props) => {
+   const [initialValues, setValue] = useState({
+      name: '',
+      firstName: '',
+      email: '',
+      age: '',
+      sex: '',
+      content: '',
+   });
 
-  const [signInFormValues, setSignInFormValues] = useState({
-    name: '',
-    firstName: '',
-    email: '',
-    password: '',
-    password2: '',
-  })
+   const [signInFormValues, setSignInFormValues] = useState({
+      name: '',
+      firstName: '',
+      email: '',
+      password: '',
+      password2: '',
+   });
 
-  return(
-    <UserContext.Provider value={
-      [initialValues, setValue],
-      [signInFormValues, setSignInFormValues]
-      }>
-      {props.children}
-    </UserContext.Provider>
-  )
-}
+   return (
+      <UserContext.Provider value={([initialValues, setValue], [signInFormValues, setSignInFormValues])}>
+         {props.children}
+      </UserContext.Provider>
+   );
+};
