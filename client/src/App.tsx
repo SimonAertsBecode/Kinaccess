6784b2e7f931c056c82dd36e7e7bc65d31bea9f4
simon <simonaertsbecode@gmail.com>
2022-01-10 16:27:10 +0100

    moving router in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 //**Import files */
 import MainNavigation from './components/navigations/MainNavigation';
 import Routes from './routes/Routes';
+import CustomRouter from './routes/CustomRouter';
+import myHistory from './utils/history';
 
 const App = () => {
    return (
-      <>
+      <CustomRouter history={myHistory}>
          <MainNavigation />
          <Routes />
-      </>
+      </CustomRouter>
    );
 };
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -21,9 +21,7 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 
 ReactDOM.render(
    <Provider store={store}>
-      <CustomRouter history={myHistory}>
-         <App />
-      </CustomRouter>
+      <App />
    </Provider>,
    document.getElementById('root')
 );
